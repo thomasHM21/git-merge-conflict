ce770d657c3701a58647cb6fea9ee8e9d640ae87
NavidNay <navid_92@hotmail.com>
2021-02-01 21:31:39 +0100

    Added created to bankaccount

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,15 +1,22 @@
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 public class BankAccount implements AccountDetails {
     private List<Transaction> transactions;
+    private LocalDate created;
 
     public BankAccount() {
         this.transactions = new ArrayList<>();
+        this. created = LocalDate.now();
     }
 
     public List<Transaction> getTransactions() {
         return transactions;
+    }
+
+    public LocalDate getCreated() {
+        return created;
     }
 
     public void addTransaction(Transaction transaction) {
